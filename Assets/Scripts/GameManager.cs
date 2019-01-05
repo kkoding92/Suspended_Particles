92a03453deb825ffbb4be8d80f5936bcf113cc54
kt0811 <hansam2396@gmail.com>
2019-01-06 02:26:11 +0900

    게임매니저 기본 로직 추가

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MaskState { basic=0, KF80, KF94, KF99 }
+
+public class GameManager : MonoBehaviour
+{
+    //보유한 코인 수
+    private ulong coin = 0;
+    //보유한 다이아몬드 수
+    private int diamond;
+    //체내 미세먼지 수
+    private float fineDustLevel;
+    //최대 체내 미세먼지 
+    private int maxFineDustLevel;
+    //밖에서 머물 수 있는 시간
+    private int stayTime = 60;
+    //마스크 보유 현환
+    private MaskState maskState = MaskState.basic;
+    //특정 미세먼지 증가시간 
+    [SerializeField] private float fineDustCheckTime = 10;
+    //특정 코인 증가시간 
+    [SerializeField] private float coinCheckTime = 20;
+
+    //클렌징 보유 여부
+    private bool checkCleansing = false;
+    //방충망 필터 보유 여부
+    private bool checkFilter = false;
+    //방안 내 산소 유무
+    private bool checkOxygen = true;
+    //공기청정기 유무
+    private bool checkAirCleaner = false;
+    //창문이 열렸는지 여부
+    private bool isOpen = true;
+
+    void Start()
+    {
+        StartCoroutine(IncreseFineDustLevel());
+        StartCoroutine(IncreseCoin());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Ventialation()
+    {
+    }
+
+    public void Wash()
+    {
+    }
+
+    public void CheckMask(MaskState newMaskState)
+    {
+        maskState = newMaskState;
+
+        switch (newMaskState)
+        {
+            case MaskState.basic:
+                stayTime = 60;
+                break;
+            case MaskState.KF80:
+                stayTime = 70;
+                break;
+            case MaskState.KF94:
+                stayTime = 80;
+                break;
+            case MaskState.KF99:
+                stayTime = 90;
+                break;
+        }
+    }
+
+    public void Drink()
+    {
+    }
+
+    public void Watch()
+    {
+    }
+
+    //미세먼지 증가루틴 
+    IEnumerator IncreseFineDustLevel()
+    {
+        while (true) {
+            if (isOpen)
+            {
+                if (checkFilter)
+                    fineDustLevel += 2;
+                else
+                    fineDustLevel += 5;
+            }
+            else
+                fineDustLevel++;
+
+            if(checkOxygen)
+                yield return new WaitForSeconds(fineDustCheckTime);
+            else
+                yield return new WaitForSeconds(fineDustCheckTime*0.5f);
+        }
+    }
+
+    IEnumerator IncreseCoin()
+    {
+        while (true)
+        {
+            if (checkAirCleaner)
+                coin += 5;
+            else
+                coin++;
+
+            Debug.Log("coin : " + coin + " FineDustLevel : " + fineDustLevel);
+            yield return new WaitForSeconds(coinCheckTime);
+        }
+    }
+}
